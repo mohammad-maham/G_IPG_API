23c223ff089cf329dd1bd465261ac36ff5cc2ffb
zavvar-sayid <zavvar.sayid@gmail.com>
2024-08-25 23:17:54 +0330

    1

--- BusinessLogic/Zarrinpal.cs
+++ BusinessLogic/Zarrinpal.cs
@@ -63,51 +63,43 @@ namespace G_IPG_API.BusinessLogic
 
         public string Payment(LinkRequest model)
         {
-            var merchant_id = _configuration.GetSection("Configuration:Zarrinpal:merchant").Value!;
 
             string amount = model.Price.ToString();
             string description = model.Title;
             string mobile = model.ClientMobile;
-            string callbackurl = model.CallbackUrl;
+            string callbackUrl = _configuration.GetSection("Configuration:Zarrinpal:CallbackUrl").Value!;
+            var merchantId = _configuration.GetSection("Configuration:Zarrinpal:Merchant").Value!;
 
-            var Parameters = new ZarrinRequestParameters(merchant_id, amount, description, callbackurl, mobile, "");
-
-            var client = new RestClient(URLs.requestUrl);
-
-            Method method = Method.Post;
-
-            var request = new RestRequest("", method);
+            var Parameters = new ZarrinRequestParameters(merchantId, amount, description, callbackUrl, mobile, "");
+            var requestUrl = _configuration.GetSection("Configuration:Zarrinpal:RequestUrl").Value!;
+            var client = new RestClient(requestUrl);
+            var request = new RestRequest("", Method.Post);
 
             request.AddHeader("accept", "application/json");
-
             request.AddHeader("content-type", "application/json");
-
             request.AddJsonBody(Parameters);
 
             var response = client.ExecuteAsync(request);
-
             return response.Result.Content;
         }
 
         public string VerifyPayment(string authority, string amount)
         {
-            VerifyParameters parameters = new VerifyParameters();
-
-            parameters.authority = authority;
-            parameters.amount = amount;
-            parameters.merchant_id = _configuration.GetSection("Configuration:Zarrinpal:merchant").Value!;
-
-            var client = new RestClient(URLs.verifyUrl);
-            Method method = Method.Post;
-            var request = new RestRequest("", method);
+            var merchantId = _configuration.GetSection("Configuration:Zarrinpal:Merchant").Value!;
+            var verifyUrl = _configuration.GetSection("Configuration:Zarrinpal:VerifyUrl").Value!;
+            var client = new RestClient(verifyUrl);
+            var request = new RestRequest("", Method.Post);
 
             request.AddHeader("accept", "application/json");
-
             request.AddHeader("content-type", "application/json");
-            request.AddJsonBody(parameters);
+            request.AddJsonBody(new VerifyParameters
+            {
+                authority = authority,
+                amount = amount,
+                merchant_id = merchantId
+            });
 
             var response = client.ExecuteAsync(request);
-
             return response.Result.Content;
         }
     }
